eb0dfc2726fe1643845e6f6f5ace63bdc4c437cb
SHArtyom <sartiom1@gmail.com>
2023-01-22 20:11:01 +0300

    Code refactoring after 3rd revision, part 2/2

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,7 +1,7 @@
 def to_str(value):
     if isinstance(value, dict):
         return '[complex value]'
-    elif isinstance(value, bool) or value == 0:
+    elif isinstance(value, bool):
         return str(value).lower()
     elif value is None:
         return 'null'
